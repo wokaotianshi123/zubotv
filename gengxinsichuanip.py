056919b8d297d2316d497676f41d7500c1062998
wokaotianshi123 <56501836+wokaotianshi123@users.noreply.github.com>
2024-04-27 08:22:05 +0800

    Update gengxinsichuanip.py

--- gengxinsichuanip.py
+++ gengxinsichuanip.py
@@ -3,8 +3,8 @@ import re
 import cv2  # 导入OpenCV库
 
 # 定义fofa链接
-fofa_url = 'https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjaXR5PSJDaGVuZ2R1Ig%3D%3D'
-# fofa_url = 'http://tonkiang.us/hoteliptv.php?page=1&pv=%E5%9B%9B%E5%B7%9D%E7%94%B5%E4%BF%A1'
+# fofa_url = 'https://fofa.info/result?qbase64=InVkcHh5IiAmJiBjaXR5PSJDaGVuZ2R1Ig%3D%3D'
+fofa_url = 'http://tonkiang.us/hoteliptv.php?page=1&pv=%E5%9B%9B%E5%B7%9D%E7%94%B5%E4%BF%A1'
 
 
 # 尝试从fofa链接提取IP地址和端口号，并去除重复项
